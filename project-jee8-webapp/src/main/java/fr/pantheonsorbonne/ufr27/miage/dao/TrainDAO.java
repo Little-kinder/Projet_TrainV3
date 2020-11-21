29b7e1c23eb677198a22405e7fec473e41e0e2f1
hfouda <hagarfouda@hotmail.fr>
2020-11-21 18:28:12 +0200

    DAO3

--- project-jee8-webapp/src/main/java/fr/pantheonsorbonne/ufr27/miage/dao/TrainDAO.java
+++ project-jee8-webapp/src/main/java/fr/pantheonsorbonne/ufr27/miage/dao/TrainDAO.java
@@ -13,6 +13,7 @@ import javax.persistence.EntityManager;
 
 import fr.pantheonsorbonne.ufr27.miage.exception.NoSuchUserException;
 import fr.pantheonsorbonne.ufr27.miage.jpa.Arret;
+import fr.pantheonsorbonne.ufr27.miage.jpa.Billet;
 import fr.pantheonsorbonne.ufr27.miage.jpa.Contract;
 import fr.pantheonsorbonne.ufr27.miage.jpa.Customer;
 import fr.pantheonsorbonne.ufr27.miage.jpa.Invoice;
@@ -53,7 +54,7 @@ public class TrainDAO {
 		return currtrain.isEtat();
 	}
 	
-	public String typeTrain(int idTrain) throws IOException{
+	public String getTypeTrain(int idTrain) throws IOException{
 
 		Train currtrain = em.find(Train.class, idTrain);
 		if (currtrain == null) {
@@ -80,27 +81,22 @@ public class TrainDAO {
 		return chemin;
 	}
 	
-	/* public List<TrainPhysique>  getTrainPhyPassager(int idPassager){
+	public List<TrainPhysique> getTrainPhyPassager(int idPassager){
 		
 		
 		Passager passager = em.find(Passager.class, idPassager);
-		Billet billet = em.find(Billet.class, ) */
+		List<Integer> listeID_TP = null;
+		listeID_TP = em.createNativeQuery("select idTrainPhysique from Billet where idPassager = idPassager")
+				.setParameter("idPassager", idPassager).getResultList();
 		
+		List<TrainPhysique> TrainPhyPassager = null; 
 		
+		for(int i : listeID_TP) {
+			TrainPhyPassager.add(em.find(TrainPhysique.class, i));
+		}
+
+		return TrainPhyPassager;
 		
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	} 
 	
 }
